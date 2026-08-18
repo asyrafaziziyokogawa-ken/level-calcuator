74eca7282b80a31e5608079feb6f4c63ed0878aa
asyrafaziziyokogawa-ken <asyrafazizi.yokogawa@gmail.com>
2026-08-18 16:02:52 +0800

    Fix field reference from dptElevation to h2 in calculator

--- js/calculator.js
+++ js/calculator.js
@@ -31,7 +31,7 @@ const Calculator = {
             elevation:
                 Number(
                     document.getElementById(
-                        "dptElevation"
+                        "h2"
                     ).value
                 ),
 
